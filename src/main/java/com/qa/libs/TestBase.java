544b2736f852bf17eba977f0dcf24ee01f36cf57
PavanGIT <pavan.pegallapati@gmail.com>
2019-12-25 17:53:41 +0530

    my first code

---
+++ src/main/java/com/qa/libs/TestBase.java
@@ -0,0 +1,14 @@
+package com.qa.libs;
+
+import org.testng.annotations.Test;
+
+public class TestBase {
+	
+	
+	@Test
+	void test()
+	{
+		System.out.println("testbase");
+	}
+
+}
---
+++ src/test/java/com/qa/pages/Home.java
@@ -0,0 +1,11 @@
+package com.qa.pages;
+
+public class Home {
+	
+	
+	void home()
+	{
+		System.out.println("home");
+	}
+
+}
